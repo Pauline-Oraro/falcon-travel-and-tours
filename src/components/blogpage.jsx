4719884a338b0d0d2c32e0bfda53fc84b8a869c3
Pauline-Oraro <hiltrapauline@gmail.com>
2025-06-16 18:13:22 +0300

    updating the blog page

--- src/components/blogpage.jsx
+++ src/components/blogpage.jsx
@@ -8,7 +8,7 @@ import BlogFour from "../assets/images/blog4.jpg";
 
 const BlogPage = () =>{
     return(
-        <div className="blogs">
+        <div>
 
             <div className="hero-section">
                 <img src={HeroImage} alt="hero section image"/>
@@ -22,44 +22,62 @@ const BlogPage = () =>{
             <div className="blogs-section">
                 <div className="blog-body">
                     <img src={BlogOne} alt="Seasonal Safari Guide: When to Visit Kenya and Tanzania for the Best Wildlife Experiences"/>
-                    <h2>Seasonal Safari Guide: When to Visit Kenya and Tanzania for the Best Wildlife Experiences</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ad aliquid cupiditate modi veritatis</p>
-                    <p className="link">View post</p>
+                    <div className="blog-statements">
+                        <h2>Seasonal Safari Guide: When to Visit Kenya and Tanzania for the Best Wildlife Experiences</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ad aliquid cupiditate modi veritatis</p>
+                        <p className="link">VIEW POST</p>
+                    </div>
+                    
                 </div>
 
                 <div className="blog-body">
                     <img src={BlogTwo} alt="Top 10 Must-Visit Safari Parks in Kenya and Tanzania" />
-                    <h2>Top 10 Must-Visit Safari Parks in Kenya and Tanzania</h2>
+                    <div className="blog-statements">
+                        <h2>Top 10 Must-Visit Safari Parks in Kenya and Tanzania</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ad aliquid cupiditate modi veritatis</p>
-                    <p className="link">View post</p>
+                    <p className="link">VIEW POST</p>
+                    </div>
+                    
                 </div>
 
                 <div className="blog-body">
                     <img src={BlogThree} alt="The Big Five Safari: Where to Spot Them in Kenya and Tanzania" />
-                    <h2>The Big Five Safari: Where to Spot Them in Kenya and Tanzania</h2>
+                    <div className="blog-statements">
+                        <h2>The Big Five Safari: Where to Spot Them in Kenya and Tanzania</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ad aliquid cupiditate modi veritatis</p>
-                    <p className="link">View post</p>
+                    <p className="link">VIEW POST</p>
+                    </div>
+                    
                 </div>
 
                 <div className="blog-body">
                     <img src={BlogFour} alt="Packing for an African Safari: Essentials for Kenya and Tanzania" />
-                    <h2>Packing for an African Safari: Essentials for Kenya and Tanzania</h2>
+                    <div className="blog-statements">
+                        <h2>Packing for an African Safari: Essentials for Kenya and Tanzania</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ad aliquid cupiditate modi veritatis</p>
-                    <p className="link">View post</p>
+                    <p className="link">VIEW POST</p>
+                    </div>
+                    
                 </div>
 
                 <div className="blog-body">
                     <img src={BlogOne} alt="Client Spotlight: Their Unforgettable Safari Adventure" />
-                    <h2>Client Spotlight: Their Unforgettable Safari Adventure</h2>
+                    <div className="blog-statements">
+                        <h2>Client Spotlight: Their Unforgettable Safari Adventure</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ad aliquid cupiditate modi veritatis</p>
-                    <p className="link">View post</p>
+                    <p className="link">VIEW POST</p>
+                    </div>
+                    
                 </div>
 
                 <div className="blog-body">
                     <img src={BlogOne} alt="Life-Changing Moments on Safari: Stories from the Wild" />
-                    <h2>Life-Changing Moments on Safari: Stories from the Wild</h2>
+                    <div className="blog-statements">
+                        <h2>Life-Changing Moments on Safari: Stories from the Wild</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ad aliquid cupiditate modi veritatis</p>
-                    <p className="link">View post</p>
+                    <p className="link">VIEW POST</p>
+                    </div>
+                    
                 </div>
             </div>
         </div>
